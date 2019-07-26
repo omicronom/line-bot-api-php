ec47e6d6d29e9aec15ca5c484ec565691e191f5f
omicronom <53294352+omicronom@users.noreply.github.com>
2019-07-26 09:54:38 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "OmIOT/"; //enter your appid
+		$KEY = "QJyK1tZHaJyvQoS"; //enter your web key
+		$SECRET = "s10GnRS1wgRyuFJxtHtRm3ryv"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
